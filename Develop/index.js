e1b0dddf76b319cbc3ac861e2e8e7c088407b64d
Linus41 <schief.craft@gmail.com>
2020-04-07 14:06:21 -0700

    github image rendered in index.html

--- Develop/index.js
+++ Develop/index.js
@@ -1,59 +1,27 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
-const api = require("./utils/api");
-const markDown = require("./utils/generateMarkdown");
-// this var holds all questions prompts and writes answers to the index.js
-const questions = [
-    inquirer.prompt([{
-        type: "input",
-        message: "What is your Github username?",
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt({
+        message: "Enter your GitHub username:",
         name: "username"
-    }]).then(answers => {
-        // answers is a variable that holds new html elements. Uses template literals with dot notation to insert user input 
-        const bioAnswers = `
-       <div>
-       <p>
-       <h2 class="username" style="color:pink;">${answers.username}</h2>
-       <p class="location">${answers.location}</p>
-       <p class="email">${answers.email}</p>
-       </p>
-    </div>
-  `;
-        // within main code block, index.html is created and the html elements and contents are inserted via bioanswers variable
-        fs.writeFile("index.html", bioAnswers, () => console.log("File written"));
-    }),
-];
+    })
+    .then(function ({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
 
-function writeToFile(fileName, data) {
-}
+        axios.get(queryUrl).then(function (res) {
+            console.log(res.data.avatar_url);
+            const image = res.data.avatar_url;
+            const imageToHTML = `
+        <div>
+        <img class="bio-image" style="width:400px;height:500px;" src="${image}">
+        </div>`;
+            fs.writeFile("index.html", imageToHTML, function (err) {
+                if (err) {
+                    throw err;
+                }
 
-function init() {
-
-}
-
-init();
-// sample axios call grabbed from Day 3 activity 29
-// inquirer
-//   .prompt({
-//     message: "Enter your GitHub username:",
-//     name: "username"
-//   })
-//   .then(function({ username }) {
-//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-
-//     axios.get(queryUrl).then(function(res) {
-//       const repoNames = res.data.map(function(repo) {
-//         return repo.name;
-//       });
-
-//       const repoNamesStr = repoNames.join("\n");
-
-//       fs.writeFile("repos.txt", repoNamesStr, function(err) {
-//         if (err) {
-//           throw err;
-//         }
-
-//         console.log(`Saved ${repoNames.length} repos`);
-//       });
-//     });
-//   });
+            })
+        });
+    });
