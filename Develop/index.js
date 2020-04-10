568c70f26f1a02b0ec319f578a728323390072ab
Linus41 <schief.craft@gmail.com>
2020-04-10 16:03:57 -0700

    add all question prompts. Add/test table of contents

--- Develop/index.js
+++ Develop/index.js
@@ -1,27 +1,97 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const questionList = [{
+    type: "input",
+    message: "Enter your GitHub username:",
+    name: "username"
+},
+{
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+},
+{
+    type: "input",
+    message: "Please enter project description",
+    name: "description",
+},
+{
+    type: "input",
+    message: "Are there any required installations for the project?",
+    name: "installation",
+},
+{
+    type: "input",
+    message: "What is the usage?",
+    name: "usage",
+},
+{
+    type: "input",
+    message: "Would you like to include a license?",
+    name: "license"
+},
+{
+    type: "input",
+    message: "Who is contributing to this project?",
+    name: "contributing"
+},
+{
+    type: "input",
+    message: "Are there any tests?",
+    name: "tests"
+},
+{
+    type: "input",
+    message: "Are there any questions?",
+    name: "questions",
+}];
 
 inquirer
-    .prompt({
-        message: "Enter your GitHub username:",
-        name: "username"
-    })
-    .then(function ({ username }) {
-        const queryUrl = `https://api.github.com/users/${username}`;
+    .prompt(questionList)
+
+    .then(function (answers) {
+        console.log(answers);
+        let data = {...answers}
+        const queryUrl = `https://api.github.com/users/${data.username}?access_token=0522d8b8fbe535ccf73858e255321a8a37f0716b`;
 
         axios.get(queryUrl).then(function (res) {
-            console.log(res.data.avatar_url);
+            console.log(res.data);
+            const email = res.data.email;
+            console.log(email);
             const image = res.data.avatar_url;
-            const imageToHTML = `
+            const infoToHTML = `
         <div>
         <img class="bio-image" style="width:400px;height:500px;" src="${image}">
+        <p class="email" style="font-size: 24px;">${email}</p>
+        <h1>${data.title}</h1>
+        <p>${data.description}</p>
+        <div>Table of Contents
+            <ol>
+                <li><a href="#installation">Installation</a></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ol>
+        </div>
+        <p id='installation'>
+        <h1>Installation: </h1>${data.installation}</p>
+        <h1></h1>
+        <h1></h1>
         </div>`;
-            fs.writeFile("index.html", imageToHTML, function (err) {
+        // make prompt to ask a series of questions and store user responses
+        // make function that generates html? 
+
+            fs.writeFile("index.html", infoToHTML, function (err) {
                 if (err) {
                     throw err;
                 }
 
             })
         });
+        
     });
